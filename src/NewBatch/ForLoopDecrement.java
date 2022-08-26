20bcfcb86f90180e39ac225b34b927deae9648cc
Vihan <Vihan@VIYONA-COMPUTER.attlocal.net>
2022-08-26 16:06:14 -0500

    ForLoop

---
+++ src/NewBatch/ForLoopDecrement.java
@@ -0,0 +1,15 @@
+package NewBatch;
+
+public class ForLoopDecrement {
+
+	public static void main(String[] args) {
+		
+		int y = 10;
+		for( y=0; y<=9; y--)
+		{
+			System.out.println(y);
+		}
+
+	}
+
+}
---
+++ src/NewBatch/ForLoopIncrement.java
@@ -0,0 +1,15 @@
+package NewBatch;
+
+public class ForLoopIncrement {
+
+	public static void main(String[] args) {
+		
+		int x=10;
+		for(x=1; x<=11; x++) {
+			System.out.println(x);
+		}
+		
+		
+	}
+
+}
