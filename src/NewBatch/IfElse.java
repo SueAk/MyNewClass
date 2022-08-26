826747a2df72fd78e3c1d2a5ce881d4a7a737f61
Vihan <Vihan@VIYONA-COMPUTER.attlocal.net>
2022-08-26 15:35:09 -0500

    IfElse

---
+++ src/NewBatch/IfElse.java
@@ -0,0 +1,17 @@
+package NewBatch;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+
+		int s=30;
+		if (s % 3==0){
+		System.out.println("Your Number Is Odd");
+		}
+		else 
+		{
+			System.out.println("Your Number Is Even");
+		}
+	}
+
+}
---
+++ src/NewBatch/IfElseNested.java
@@ -0,0 +1,11 @@
+package NewBatch;
+
+public class IfElseNested {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
